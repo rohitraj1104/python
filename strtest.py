7e0ce433f9cc458d79b35ae664b8c689efa8d15e
Rohit <rohit@Rohits-MacBook-Pro.local>
2021-09-16 19:59:38 +0530

    string update 2

--- strtest.py
+++ strtest.py
@@ -18,5 +18,18 @@ print(E)
 F = "__".join(D)
 print(F)
 
+x = "cisco"
+y = "switch"
+z= x+x    #string with operand
+print(z)
+k= x*3    #string with operand
+print(k)
+if('r' in x):  #string with true/false check
+    print("yes,its there")
 
+#================================
+print("my name is : %s and I am %d years old and my weight is %f" % (A,30,83.5))
+
+print("my name is : {} and I am {} years old and my weight is {}".format(A,30,83.5))
+#=================================
 
